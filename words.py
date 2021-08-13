64c6ac1b957e63c528d606cf2b4453a2ecc6a57c
John (Jef) Fisher <Fishe545@gmail.com>
2021-08-13 13:36:57 +0800

    first commit

---
+++ words.py
@@ -0,0 +1,11 @@
+def print_upper_words(words):
+    """Print each word in the words list in uppercase and on a newline
+
+    Args:
+        words (list): list of strings
+    """
+    for word in words:
+        print(word.upper())
+        
+        
+print_upper_words(['fish', 'cat','dog','duck'])
