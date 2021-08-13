dc0c6d6eca028f1937cb8a58727c54b06326b88c
John (Jef) Fisher <Fishe545@gmail.com>
2021-08-13 14:00:19 +0800

    feat: tweaked function to accept a set of letters and only print words that start with said letters

--- words.py
+++ words.py
@@ -1,12 +1,15 @@
-def print_upper_words(words):
+def print_upper_words(words, must_start_with):
     """Print each word in the words list in uppercase and on a newline
 
     Args:
         words (list): list of strings
+        must_start_with (set): set of letters
     """
     for word in words:
-        if word[0] == 'e' or word[0] == 'E':
-            print(word.upper())
-        
-        
-print_upper_words(['fish', 'cat','dog','duck', 'eagle'])
+        first_letter = word.lower()[0]
+        for char in must_start_with:
+            if first_letter == char:
+                print(word.upper())
+
+print_upper_words(["hello", "hey", "goodbye", "yo", "yes"],
+                   must_start_with={"h", "y"})
